fd7eed200c4eaa8f1c012e41c6212a0050a97026
Daniel Peter <daniel.peter@me.com>
2021-04-19 18:50:46 +0200

    Adds cologne presentation

---
+++ Examples/LiveCodingExampleApp/LiveCodingExampleApp/Deeplinks/DetailDeeplinkHandler.swift
@@ -0,0 +1,1 @@
+/// TODO: Handle detail?id=0
--- Examples/LiveCodingExampleApp/LiveCodingExampleApp/LiveCodingExampleAppApp.swift
+++ Examples/LiveCodingExampleApp/LiveCodingExampleApp/LiveCodingExampleAppApp.swift
@@ -1,25 +1,35 @@
 import ComposableArchitecture
 import SwiftUI
 
-let appStore: Store<SandwichOverviewState, SandwichOverviewAction> = {
-    Store<SandwichOverviewState, SandwichOverviewAction>(
-        initialState: SandwichOverviewState(
-            sandwiches: [
-                .danielsFavorite
-            ] + (0..<20).map { _ in Sandwich.random() }
-        ),
-        reducer: sandwichOverviewReducer,
-        environment: SandwichOverviewEnvironment()
-    )
-}()
-
 @main
 struct LiveCodingExampleAppApp: App {
+    let appStore: Store<SandwichOverviewState, SandwichOverviewAction>
+
+    init() {
+        // TODO: Define datasource
+
+        // TODO: Init navigator
+
+        // TODO: Define deeplink handler
+
+        appStore = Store(
+            initialState: SandwichOverviewState(
+                sandwiches: [
+                    .danielsFavorite
+                ] + (0..<20).map { _ in Sandwich.random() }
+            ),
+            reducer: sandwichOverviewReducer,
+            environment: SandwichOverviewEnvironment()
+        )
+    }
+
     var body: some Scene {
         WindowGroup {
+            // TODO: Use ComposableNavigator
             NavigationView {
                 SandwichOverviewView(store: appStore)
             }
+            // TODO: handle Deeplinks
         }
     }
 }
